8a9dcbf9e549e6d23c8827790e055c58fa34210b
Bla1 <rnbest@hotmail.com>
2017-02-26 07:47:44 -0600

    Update CareGiver.js

--- CareGiver.js
+++ CareGiver.js
@@ -1,6 +1,43 @@
-exports.handler = (event, context, callback) => {
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+
+var params = {
+		Item: {
+			medicine: [],
+			userID: 1
+			    
+		},
+		TableName: 'medicalinfo',
+		Key: {
+			    "userID": 1
+			}
+
+	};
+
+exports.handler = function(event, context, callback) {
 	
-	//try catch is for potential errors in code
+	// Initialize variables
+	
+	/*let params = {
+		TableName: 'medicalinfo',
+		Key: {
+			"userID": "one";
+		}
+	};*/
+
+	docClient.get(params, function(err,data){
+		if(err){
+			callback(err,null);
+		}else{
+			callback(null,data);
+		}
+	});
+
+	var sendString = "";
+    
+
+
+	// Try catch is for potential errors in code
 	try {
 
 		if (event.session.new) {
@@ -15,22 +52,149 @@ exports.handler = (event, context, callback) => {
 				console.log('LAUNCH REQUEST')
 				context.succeed(
 					generateResponse(
-						{"medicine": []},
-						buildSpeechletResponse(event.request.intent.name, "Welcome to Ferris' example Alexa. This runs on Lambda", "", true)
-						)
+						{},
+						buildSpeechletResponse(
+							"Hello, my name is Butler, I am here to assist you in your daily needs.",false)
 					)
+				)
 				break;
+
+
 
 			case "IntentRequest":
 				// Intent Request *** called if specified call is made to intent
 				console.log('INTENT REQUEST')
-				context.succeed(
-					handleIntent(event.request, event.session),
-						(sessionAttributes, speechletResponse=>callback(null, buildResponse(sessionAttributes, speechletResponse));
+                var noDuplicateDrugFlag = true;
+				//checks specific intent requests
+				switch(event.request.intent.name) {
+				
+					case "StoreMedicineInfo": 
+					    //console.log("you are here");
+					    docClient.get(params, function(err,data){
+							if(err){
+								callback(err,null);
+							}else{
+								callback(null,data);
+							}
 						});
-					);
+					    for(var i = 0; i< params.Item.medicine.length; i++){
+					        if(params.Item.medicine[i]==event.request.intent.slots.Drugs.value){
+					        sendString = "You have already added the drug " + event.request.intent.slots.Drugs.value;
+					        context.succeed(
+						       	generateResponse(
+						       		{},
+							       	buildSpeechletResponse(sendString,false)
+					        		)
+    		    				)
+        					noDuplicateDrugFlag = false;
+        					break;
+					        }
+					    }
+					    if(noDuplicateDrugFlag){
+					    sendString = "You need to take "
+					    
+					    
+					    params.Item.medicine[params.Item.medicine.length] = event.request.intent.slots.Drugs.value;
+						//medicine[medicine.length] = event.intent.slots.Drugs.value;
+						//console.log("you are leaving");
+						for(var i = 0; i < params.Item.medicine.length - 1; i++) {
+							sendString += params.Item.medicine[i] + " ";
+						}
+						if(params.Item.medicine.length!=1)
+						sendString += "and ";
+						sendString += params.Item.medicine[params.Item.medicine.length-1];
+						context.succeed(
+							generateResponse(
+								{},
+								buildSpeechletResponse(sendString,false)
+							)
+						)
+						docClient.put(params, function(err, data) {
+	                    	if(err) {
+                			callback(err, null);
+                    		}else {
+                    			callback(null, data);
+	                    	}
+	                        });
+					    }
+						break;
+
+					case "GetMedicineInfo":
+						docClient.get(params, function(err,data){
+							if(err){
+								callback(err,null);
+							}else{
+								callback(null,data);
+							}
+						});
+						sendString = "You still need to take ";
+						for(var i = 0; i < params.Item.medicine.length - 1; i++) {
+							sendString += params.Item.medicine[i] + " ";
+						}
+						if(params.Item.medicine.length!=1)
+						sendString += "and ";
+						sendString += params.Item.medicine[params.Item.medicine.length-1];
+						if(params.Item.medicine.length==0)
+						sendString = "You do not need any medications";
+						context.succeed(
+							generateResponse(
+								{},
+								buildSpeechletResponse(sendString,false)
+							)
+						)
+						break;
+                    
+                    
+                    case "DeleteMedicineInfo":
+						docClient.get(params, function(err, data) {
+							if(err){
+								callback(err,null);
+							}else{
+								callback(null,data);
+							}
+						});
+
+						var d = params.Item.medicine.indexOf(event.request.intent.slots.Drugs.value);
+
+						var n = [];
+
+						var deleted = params.Item.medicine[d];
+
+						for (var j = 0; j < params.Item.medicine.length; j++) {
+							if (j != d) n.push(params.Item.medicine[j]);
+						}
+
+						params.Item.medicine = n;
+
+						docClient.put(params, function(err, data) {
+	                    	if(err) {
+                			callback(err, null);
+                    		} else {
+                    			callback(null, data);
+	                    	}
+	                    });
+
+	                    context.succeed(
+							generateResponse(
+								{},
+								buildSpeechletResponse("Deleted " + deleted, false)
+							)
+						)
+			            break;
+                    
+					default:
+					    context.succeed(
+							generateResponse(
+								{},
+								buildSpeechletResponse("I'm sorry. I do not understand. Please try again",true)
+							)
+						)
+						//throw "Invalid intent"
+				}
 
 				break;
+
+
 
 			case "SessionEndedRequest":
 				// Session Ended Request *** called if code is exited
@@ -39,36 +203,47 @@ exports.handler = (event, context, callback) => {
 
 			default:
 				context.fail('INVALID REQUEST TYPE: ${event.request.type}')
-
 		}
 
-	} catch(error) { context.fail('Exception: ${error}') }
 
+
+	} catch(error) { context.fail('Exception: ${error}') }
+	
+//}
 }
 
 // Helpers
-buildSpeechletResponse = (title, outputText, repromptText, shouldEndSession) => {
+buildSpeechletResponse = (outputText, shouldEndSession) => {
 
 	return {
 		outputSpeech: {
 			type: "PlainText",
 			text: outputText
 		},
-		// Add for ability to communicate with phone app
-		// card: {
-        //     type: 'Simple',
-        //     title: `SessionSpeechlet - ${title}`,
-        //     content: `SessionSpeechlet - ${output}`,
-        // },
-		reprompt: {
-		    outputSpeech: {
-			type: 'PlainText',
-			text: repromptText,
-		    },
-		},
-		shouldEndSession
-	};
+		shouldEndSession: shouldEndSession
+	}
 }
+
+/*buildSpeechletResponse = (title, output, repromptText, shouldEndSession) => {
+    return {
+        outputSpeech: {
+            type: 'PlainText',
+            text: output,
+        },
+        card: {
+            type: 'Simple',
+            title: `SessionSpeechlet - ${title}`,
+            content: `SessionSpeechlet - ${output}`,
+        },
+        reprompt: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: repromptText,
+            },
+        },
+        shouldEndSession: shouldEndSession,
+    };
+}*/
 
 generateResponse = (sessionAttributes, speechletResponse) => {
 
@@ -78,73 +253,3 @@ generateResponse = (sessionAttributes, speechletResponse) => {
 		response: speechletResponse
 	}
 }
-
-handleIntent = (intentRequest, session, callback) => {
-	console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
-	const intent = intentRequest.intent;
-	const intentName = intent.name;
-
-	// if (intentName == /* INTENT NAME HERE */) {
-		// call intent function
-		// function(intent, session, callback) or
-		// function(callback)
-	// }
-	if (intentName == 'getMeds') {
-		getMeds(intent, session, callback);
-	} else if (intentName === 'AMAZON.HelpIntent') {
-        // function here
-		//pass;
-	} else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
-        // function here
-		//pass;
-	} else {
-        throw new Error('Invalid intent');
-	}
-
-}
-
-
-/// /  intents  / ///
-
-getMeds = (intent, session, callback) => {
-	const cardTitle = intent.name;
-	const endSession = false;
-	const reprompt = '';
-	let sessionAttributes = session.attributes;
-	let response = '';
-
-	if (sessionAttributes.length > 0) {
-		const person = personSlot.value;
-		
-		response = "";
-		
-		for (i = 0; i < sessionAttributes.length; i++) {
-			med = sessionAttributes[i];
-			response.concat("${med} ");
-		}
-	} else {
-		response = "No medications";
-	}
-
-	callback(sessionAttributes, buildSpeechletResponse(cardTitle, response, reprompt, endSession));
-}
-
-addMed = (intent, session, callback) => {
-	const cardTitle = intent.name;
-	const medSlot = intent.slots.Medicine;
-	const endSession = false;
-	const reprompt = '';
-	let sessionAttributes = session.attributes;
-	let response = '';
-	
-	if (medSlot) {
-		const med = medSlot.value;
-		sessionAttributes.push(med);
-		response = "Added ${med}.";
-	} else {
-		response = "I don't understand.";
-	}
-	
-	callback(sessionAttributes, buildSpeechletResponse(cardTitle, response, reprompt, endSession));
-}
-/// /           / ///
